1135535fad809cbb2238de9a106cd39235152d5e
Jatin Kamboj <kumar.jatin873@gmail.com>
2025-09-01 10:48:21 +0530

    Update: fix the data

--- src/api/contest/services/contest.js
+++ src/api/contest/services/contest.js
@@ -9,10 +9,17 @@ const { createCoreService } = require('@strapi/strapi').factories;
 
 module.exports = createCoreService('api::contest.contest', ({ strapi }) => ({
     fetchActiveContest: async () => {
-        return strapi.db.query('api::contest.contest').findMany({
+        let data = await strapi.db.query('api::contest.contest').findMany({
             where: {
                 isActive: true
             }
         });
+
+        return data.map(contest => {
+            return {
+                id: contest.documentId,
+                name: contest.name
+            }
+        })
     }
 }));
