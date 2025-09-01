a2288064d719be3fda567e32354862701b68387b
Jatin Kamboj <kumar.jatin873@gmail.com>
2025-09-01 10:35:32 +0530

    Update: Contest Public API

--- src/api/contest/routes/contest.js
+++ src/api/contest/routes/contest.js
@@ -7,31 +7,4 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-const defaultRouter = createCoreRouter('api::contest.contest');
-
-
-const myExtraRoutes = [{
-    method: 'GET',
-    path: '/contests',
-    handler: 'api::contest.contest.fetchActiveContest',
-    config: {
-        auth: false,
-        policies: []
-    }
-}];
-
-const customRouter = (innerRouter, extraRoutes =  []) => {
-    let routes;
-    return {
-        get prefix() {
-            return innerRouter.prefix;
-        },
-        get routes() {
-            if (!routes) routes = innerRouter.routes.concat(extraRoutes);
-            return routes;
-        }
-    }
-};
-
-module.exports = customRouter(defaultRouter, myExtraRoutes);
-
+module.exports = createCoreRouter('api::contest.contest');
